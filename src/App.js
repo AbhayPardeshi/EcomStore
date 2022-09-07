5f9bf7e2ac2949d1e8598b556e3e766b38f371db
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-09-07 15:55:01 +0530

    cardItem layout completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Home from "./pages/home/Home";
 import Layout from "./pages/layout/Layout";
-
+import { Route, Routes } from "react-router-dom";
+import DisplayItem from "./components/display-item-card/DisplayItem";
 function App() {
   return (
     <>
       <Layout>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item" element={<DisplayItem />} />
+          <Route path=""
+        </Routes>
       </Layout>
     </>
   );
