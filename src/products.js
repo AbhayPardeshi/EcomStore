29bf18ac2c1cfaa2bd1f1b777521bb67713a3eae
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-09-13 12:26:22 +0530

    few changes

---
+++ src/products.js
@@ -0,0 +1,19 @@
+{
+  {
+    "_id": "5",
+    "name": "SP X ADIDAS NMD 22",
+    "make": "ADIDAS",
+    "imageUrl":
+      "https://res.cloudinary.com/dtcbqzynj/image/upload/v1663051114/Shoe%20Palace/men-adidas-1_tnrwoc.webp",
+    "description": "MENS LIFESTYLE SHOES",
+    "originalPrice": "150.00",
+    "discountedPrice": "",
+    "rating": "",
+    "isLatest": true,
+    "totalRatings": "",
+    "availableSize": ["7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11"],
+    "categoryName": "MEN",
+    "color":"Blue"
+    "quantitiesInCart": 0 
+  }
+}
