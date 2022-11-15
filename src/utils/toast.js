9382c57b1a773efafd4e9d3431ea81d0dc9fb8f0
AbhayPardeshi <abhaypardeshi2210@gmail.com>
2022-11-15 14:45:06 +0530

    Update toast.js

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -1,8 +1,9 @@
-import { Slide, toast } from "react-toastify";
+import { Slide } from "react-toastify";
+import { Toast } from "react-toastify/dist/components";
 import "react-toastify/dist/ReactToastify.css";
 
 export const Toast = ({ type, msg }) => {
-  return toast(msg, {
+  return Toast(msg, {
     autoClose: 2000,
     closeOnClick: true,
     pauseOnHover: false,
